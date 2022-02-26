dd962f41a307a3ff885ea407d4cd6de1f096b88f
Aashish Rana <aashishrana71135@gmail.com>
2022-02-26 16:16:47 +0530

    try to use postman

--- models/User.js
+++ models/User.js
@@ -8,4 +8,40 @@ const UserSchema = new mongoose.Schema({
         max: 20,
         unique: true
     },
-})
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default:" "
+    },
+    followers: {
+        type: Array,
+        default: []
+    }, 
+
+    following: {
+        type: Array,
+        default: []
+    }, 
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    }
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
